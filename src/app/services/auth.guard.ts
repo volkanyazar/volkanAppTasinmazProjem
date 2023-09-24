650466e58ae0039116567353f90d6b60cf5b7404
dunda <dunda@DESKTOP-RQ33G5H>
2023-09-24 19:41:00 +0300

    fixed auth bugs.

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -19,7 +19,7 @@ export class AuthGuard implements CanActivate {
         return true;
       } 
     } else {
-      this.router.navigate(["auth/login"]);
+      this.authService.logOut();
       return false;
     }
   }
